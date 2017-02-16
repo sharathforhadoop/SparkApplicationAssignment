f28bccbf656dcaded947076a94913fb264512f58
sharathforhadoop <sharathforhadoop@gmail.com>
2017-02-16 09:32:23 -0500

    Create CalculatorOperations.java

---
+++ src/CalculatorOperations.java
@@ -0,0 +1,6 @@
+public interface CalculatorOperations {
+
+	public double add(double left, double right);
+	public double substraction(double left, double right);
+	public double multiplication(double left, double right);
+}
